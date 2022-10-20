184b20743cf26cceacdd3d3630144f5d5d036044
ldkr00 <lukasdekker77@gmail.com>
2022-10-20 18:01:47 +0200

    Create plot.py

---
+++ Homework2/plot.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+
+url = "https://raw.githubusercontent.com/ipozdeev/it-skills-for-research/master/data/coding-environment-exercise.csv"
+data = pd.read_csv(url)
+date = data.date
+value = data.value
+
+def plot_data(x,y):
+    plt.rc('font', size=12) 
+    plt.figure(figsize=(10,6), tight_layout=True)
+    plt.plot(x, y, linewidth=2)
+    plt.xticks(np.arange(0, len(date) + 1, 2))
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.title('Evolution of Unknown')
+    plt.show()
+
+if __name__ == '__main__':
+    plot_data(date, value)
+
+
