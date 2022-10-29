b55220a9db0d59fd004c04c19bb2629f587bdb78
ldkr00 <lukasdekker77@gmail.com>
2022-10-29 14:19:27 +0200

    Solved Part  2 (FRED API)

--- Homework6/request.py
+++ Homework6/request.py
@@ -1,4 +1,4 @@
-
+### Part 1 (Binance)
 # root URL for Binance API: https://api.binance.com/api/v3
 
 # endpoint to retrieve klines for ETH/USD: /klines?symbol=ETHUSDT
@@ -7,7 +7,7 @@
 import requests
 requests.get('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=75&startTime=1661990400')
 
-#Write a function that retrieves 75 observations of klines data for a generic currency pair since a generic date. The function should take the currency pair and start date as input parameters.
+# Write a function that retrieves 75 observations of klines data for a generic currency pair since a generic date. The function should take the currency pair and start date as input parameters.
 def get_data(pair, date):
     url= f'https://api.binance.com/api/v3/klines?symbol={pair}&interval=1d&limit=75&startTime={date}'
     data = requests.get(url)
@@ -15,5 +15,14 @@ def get_data(pair, date):
     assert data.status_code == 200
     return data
 
-#Check function
+# Check function
 data6 = get_data('BTCUSDT', 1661990400)
+
+
+### Part 2 (FRED)
+# root URL for FRED API: https://api.stlouisfed.org
+
+# Endpoint to retrieve time series observations: /fred/series/observations
+
+# Construct a query string to retrieve the series of the monthly unemployment rate (seasonally adjusted) since 2020-01. Use the fake API key abc123 in the query string.
+# https://api.stlouisfed.org/fred/series/observations?api_key=abc123&series_id=UNRATE&observation_start=2020-01-01&frequency=m
